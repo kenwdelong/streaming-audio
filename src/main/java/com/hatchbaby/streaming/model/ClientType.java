0c9ca3f6bae4c9d9a80930adeeb21c244466cd8c
Ken DeLong <kenwdelong@gmail.com>
2019-01-04 10:56:29 -0800

    basic transceiver

--- src/main/java/com/hatchbaby/streaming/model/ClientType.java
+++ src/main/java/com/hatchbaby/streaming/model/ClientType.java
@@ -1,18 +1,27 @@
 package com.hatchbaby.streaming.model;
 
+import org.jitsi.service.neomedia.MediaDirection;
+
 public enum ClientType
 {
-	Rx("/rx"), Tx("/tx");
+	Rx("/rx", MediaDirection.RECVONLY), Tx("/tx", MediaDirection.SENDONLY);
 	
 	private String uri;
+	private MediaDirection direction;
 	
-	private ClientType(String uri)
+	private ClientType(String uri, MediaDirection direction)
 	{
 		this.uri = uri;
+		this.direction = direction;
 	}
 	
 	public String getUri()
 	{
 		return uri;
+	}
+	
+	public MediaDirection getDirection()
+	{
+		return direction;
 	}
 }
