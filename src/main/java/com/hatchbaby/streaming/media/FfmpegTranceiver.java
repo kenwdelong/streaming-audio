dbe57b36e5b03b6789240c5aa05ac12308b71364
Ken DeLong <kenwdelong@gmail.com>
2019-03-12 15:16:45 -0700

    ffmpeg example

--- src/main/java/com/hatchbaby/streaming/media/FfmpegTranceiver.java
+++ src/main/java/com/hatchbaby/streaming/media/FfmpegTranceiver.java
@@ -1,29 +1,58 @@
 package com.hatchbaby.streaming.media;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.UnknownHostException;
 
 import com.hatchbaby.streaming.model.ClientType;
 
 public class FfmpegTranceiver extends Transceiver
 {
+	private Process process;
 
-	public FfmpegTranceiver(int localPortBase, String remoteHost, int remotePortBase, ClientType clientType) throws UnknownHostException
+	public FfmpegTranceiver(int localPortBase, String remoteHost, int remotePortBase,
+			ClientType clientType) throws UnknownHostException
 	{
 		super(localPortBase, remoteHost, remotePortBase, clientType);
 	}
+
+	// This worked locally!
+	// ffplay -f rtsp -rtsp_flags listen -v debug rtsp://127.0.0.1:50000/stream
+	// ffmpeg -re -i /tmp/Earl04.mp3 -f rtsp -allowed_media_types audio -v debug rtsp://127.0.0.1:50000/stream
 	
 	@Override
 	public void start() throws Exception
 	{
-		// TODO Auto-generated method stub
+		// Here's a more robust impl: https://alvinalexander.com/java/java-exec-processbuilder-process-1
+		try
+		{
+			ProcessBuilder pb = new ProcessBuilder("myCommand", "myArg1", "myArg2");
+			File file = new File("myDir");
+			pb.directory(file);
+			process = pb.start();
 
+//			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+//			String line = reader.readLine();
+//			while(line != null)
+//			{
+//				System.out.println(line);
+//				line = reader.readLine();
+//			}
+			logger.info("Ffmpeg started");
+		}
+		catch(IOException e1)
+		{
+			logger.error("Can't start ffmpeg", e1);
+		}
 	}
 
 	@Override
 	public void stop()
 	{
-		// TODO Auto-generated method stub
-
+		if(process != null)
+		{
+			process.destroy();
+		}
 	}
 
 }
